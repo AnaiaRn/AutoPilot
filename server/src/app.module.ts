bb4ed3492a96b474f0eae5797a0e0e76f3bd899d
Anaia-Randrianantenaina <anaiarandrianantenaina@gmail.com>
2025-05-21 20:35:35 +0300

    generate resources users, configuration db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ProjectsModule } from './projects/projects.module';
+
 
 @Module({
-  imports: [AuthModule, UsersModule, ProjectsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.BD_PORT),
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
