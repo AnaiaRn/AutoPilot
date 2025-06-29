6c6743a5d5521bd5518c9646adb86c2dbe9e14f4
Anaia-Randrianantenaina <anaiarandrianantenaina@gmail.com>
2025-06-29 21:14:02 +0300

    suppression resource plans

--- server/src/projects/projects.module.ts
+++ server/src/projects/projects.module.ts
@@ -1,17 +1,13 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
-import { PlansModule } from '../plans/plans.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Project]),
-    forwardRef(() => PlansModule), // Utilisation de forwardRef
-  ],
+  imports: [TypeOrmModule.forFeature([Project])],
   controllers: [ProjectsController],
   providers: [ProjectsService],
-  exports: [ProjectsService, TypeOrmModule], // Export du service et du repository
+  exports: [ProjectsService],
 })
 export class ProjectsModule {}
